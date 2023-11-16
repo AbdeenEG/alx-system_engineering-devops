daabeded14809bbe256920ec7d20f1055935d6b3
Abdeen <abdallah.zakaria2021@gmail.com>
2023-11-16 10:32:55 -0800

    0x1B

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
-# fix request limit at nginx
-exec { 'fix--for-nginx':
-command => 'sed -i "s/15/4096/" /etc/default/nginx',
-path    => '/usr/local/bin/:/bin'
- }
+# Fix problem of high amount of requests
 
- # Restart nginx
- -> exec { 'nginx-restart:'
-     command => 'nginx restart',
-     path => '/etc/init.d/'
- }
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
